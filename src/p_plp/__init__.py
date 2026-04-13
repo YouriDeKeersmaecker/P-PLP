01f4bc97cb3abbb9d5a0665a9117668941673394
Youri De Keersmaecker <youri.dekeersmaecker@student.hogent.be>
2026-04-13 20:58:40 +0200

    remove outdated file

--- src/p_plp/__init__.py
+++ src/p_plp/__init__.py
@@ -1,31 +1,5 @@
-from .pipeline import (
-    PipelineRunPlan,
-    build_pipeline_plan,
-    run_pipeline_config,
-    run_pipeline,
-    run_pipeline_spec,
-    run_postgres_pipeline,
-)
-from .cohorts import (
-    execute_atlas_sql,
-    load_atlas_cohort,
-    read_atlas_cohort,
-)
-from .config import PipelineRunConfig, PredictionProblemConfig
-
 __version__ = "0.0.1"
 
 __all__ = [
     "__version__",
-    "PipelineRunConfig",
-    "PredictionProblemConfig",
-    "PipelineRunPlan",
-    "execute_atlas_sql",
-    "load_atlas_cohort",
-    "read_atlas_cohort",
-    "build_pipeline_plan",
-    "run_pipeline",
-    "run_pipeline_config",
-    "run_pipeline_spec",
-    "run_postgres_pipeline",
 ]
